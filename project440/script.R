369735a0ee122010e9e6745e1f28357ac6e2ca9c
Aleksander Bentoske <ajmill38@asu.edu>
2021-03-26 16:56:15 -0700

    initial commit

---
+++ project440/script.R
@@ -0,0 +1,36 @@
+# install packages + load libraries - these are sequential 
+install.packages("remotes")
+install.packages("BiocManager")
+library(remotes)
+library(BiocManager)
+remotes::install_github("mhahsler/rMSA")
+library(rMSA)
+
+
+
+
+
+# read multi-FASTA
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
